6bcf3f1a618f1340062d50e5226ffd6d03e38f74
Ronin Sharma <rrs234@cornell.edu>
2022-01-28 17:28:35 -0500

    added service backend outlines

---
+++ services/question-answering/question.py
@@ -0,0 +1,16 @@
+
+# Functions ----
+
+# get all questions 
+
+# post a new question
+
+# update a question
+
+# add a response to a question
+
+# update a response
+
+# add a follow-up to a question
+
+# update a follow-up
---
+++ services/queueing/queue.py
@@ -0,0 +1,12 @@
+
+# Functions ----
+
+# get all entries in the queue
+
+# add entry to the queue
+
+# update an entry in the queue
+
+# get queue statistics
+
+# update queue statistics
