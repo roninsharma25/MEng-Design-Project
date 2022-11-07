c1337a8fdee461556ab2ff72131e9cb5c238cb4e
Ronin Sharma <rrs234@cornell.edu>
2022-11-07 01:00:45 -0500

    updated performance testing

--- performance/inject_requests.py
+++ performance/inject_requests.py
@@ -1,83 +1,31 @@
-from queue import Queue
-from threading import Thread
-import matplotlib.pyplot as plt
+import concurrent.futures
 import requests
 import time
+import matplotlib.pyplot as plt
+
+def func(url):
+    resp = requests.get(url)
+    return resp
+
+x = []
+y = []
+
+for i in range(1, 1001, 100):
+    x.append(i)
+    urls = ['http://localhost:5000/all'] * i
+    start_time = time.time()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=i) as pool:
+        responses = list(pool.map(func, urls))
+        
+    elapsed_time = time.time() - start_time
+    y.append(elapsed_time)
+
+    print(i, elapsed_time)
 
 
-frontend_url = 'http://localhost:3000'
-backend_url = 'http://localhost:5000'
-
-def basic_get(url):
-    url_split = url.split('/')
-    index = url_split[-1]
-    endpoint = '/'.join(url_split[:-1])
-    result = session.get(endpoint)
-
-    results[index] = result
-
-# Worker class that serves individual threads
-class Worker(Thread):
-    def __init__(self, tasks):
-        super().__init__()
-        self.tasks = tasks
-        self.daemon = True
-        self.start()
-    
-    def run(self):
-        while True:
-            func, args, kargs = self.tasks.get()
-            try:
-                func(*args, **kargs)
-            except Exception as e:
-                print(e)
-            finally:
-                self.tasks.task_done()
-
-# ThreadPool class that has the threads executing tasks from a queue
-class ThreadPool():
-    def __init__(self, num_threads):
-        self.tasks = Queue(num_threads)
-        for i in range(num_threads):
-            Worker(self.tasks)
-    
-    def add_task(self, func, *args, **kargs):
-        self.tasks.put((func, args, kargs))
-    
-    def map(self, func, args_list):
-        for args in args_list:
-            self.add_task(func, args)
-    
-    def wait_completion(self):
-        self.tasks.join()
-
-
-if __name__ == '__main__':
-    # RPS
-    x = []
-
-    # Latency
-    y = []
-
-    for i in range(1000, 20001, 1000):
-        urls = [f'{frontend_url}/{j}' for j in range(i)]
-        pool = ThreadPool(40)
-        results = {}
-        session = requests.session()
-
-        start_time = time.time()
-        pool.map(basic_get, urls)
-        pool.wait_completion()
-        total_time = time.time() - start_time
-
-        x.append(i)
-        y.append(total_time)
-
-        print(i, total_time)
-
-    # Plot Results
-    plt.plot(x, y)
-    plt.title('RPS vs Latency')
-    plt.xlabel('Requests per Second (RPS)')
-    plt.ylabel('Latency (seconds)')
-    plt.savefig('rps_latency.png')
+# Plot Results
+plt.plot(x, y)
+plt.title('RPS vs Latency')
+plt.xlabel('Requests per Second (RPS)')
+plt.ylabel('Latency (seconds)')
+plt.savefig('rps_latency.png')
